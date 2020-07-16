e73171a114b17b77cfb39b3adacf58c201e69f32
Jack Quinoa <jquinoa208@gmail.com>
2020-07-16 17:19:39 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,12 +2,12 @@
 class GetRequester 
 
 attr_accessor :url
-  def initialize(url)
+  def initialize(URL)
     @url = url 
   end
   
   def get_response_body
-    uri = URI.parse(@url)
+    uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body 
   end
